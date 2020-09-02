70df40be5d96cc83b4fcd1071deb8523eed72954
shun <shun.fix9@gmail.com>
2020-09-02 20:45:59 +0000

    refactor

--- public/lib/x_preact/load/load_script.ts
+++ public/lib/x_preact/load/load_script.ts
@@ -2,21 +2,20 @@ import { VNode } from "preact";
 import { html } from "htm/preact";
 import { useState, useEffect } from "preact/hooks";
 import { LoadScriptState } from "../../load/load_script";
+import { LoadedScript } from "../../load/script/data";
 
-interface component {
+interface PreactComponent {
     (): VNode
 }
 
-export function LoadScript(initialState: LoadScriptState): component {
+export function LoadScript(initialState: LoadScriptState): PreactComponent {
     return (): VNode => {
         const [state, setState] = useState(initialState);
 
         useEffect(() => {
             // script タグは body.appendChild しないとスクリプトがロードされないので useEffect で追加する
             if (state.state === "loaded" && state.script.success) {
-                const script = document.createElement("script");
-                script.src = state.script.path.path;
-                document.body.appendChild(script);
+                appendScript(state.script.path.path);
             }
         }, [state]);
 
@@ -24,22 +23,36 @@ export function LoadScript(initialState: LoadScriptState): component {
             case "initial":
                 state.next.then(setState);
 
-                // path の取得には時間がかからないはずなので空を返す
-                return html``
+                return viewInitial();
 
             case "loaded":
-                if (state.script.success) {
-                    // script の描画は useEffect でするので、本体は空で返す
-                    return html``
-                } else {
-                    // TODO エラー画面を用意
-                    return html`load-error: ${state.script.err.err}`
-                }
+                return viewLoaded(state.script);
 
             default:
                 return assertNever(state);
         }
     }
+}
+
+function viewInitial(): VNode {
+    // path の取得には時間がかからないはずなので空を返す
+    return html``
+}
+
+function viewLoaded(script: LoadedScript): VNode {
+    if (script.success) {
+        // script の描画は useEffect でするので、本体は空で返す
+        return html``
+    } else {
+        // TODO エラー画面を用意
+        return html`load-error: ${script.err.err}`
+    }
+}
+
+function appendScript(path: string) {
+    const script = document.createElement("script");
+    script.src = path;
+    document.body.appendChild(script);
 }
 
 function assertNever(_: never): never {
