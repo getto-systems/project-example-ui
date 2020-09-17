683c7a2be732cf1504973aa94c1646284306f5d4
shun <shun.fix9@gmail.com>
2020-09-17 11:40:29 +0000

    refactor: new password reset action

--- public/lib/z_main/auth/password_reset_session.ts
+++ public/lib/z_main/auth/password_reset_session.ts
@@ -1,53 +1,21 @@
-//import { initAuthClient } from "../../z_external/auth_client/auth_client"
-//import { env } from "../../y_static/env"
-
-import { newTimeConfig } from "./config"
+import { newPasswordResetAction } from "./action/password_reset"
 
 import {
     initPasswordResetSessionComponent,
     initPasswordResetSessionWorkerComponentHelper,
 } from "../../auth/password_reset_session/core"
 
-import { initPasswordResetAction } from "../../password_reset/impl/core"
 import { initLoginIDFieldAction } from "../../field/login_id/impl/core"
-
-import { initSimulatePasswordResetSessionClient } from "../../password_reset/impl/client/password_reset_session/simulate"
-import { initSimulatePasswordResetClient } from "../../password_reset/impl/client/password_reset/simulate"
-
-import { PasswordResetSessionClient } from "../../password_reset/infra"
-import { PasswordResetClient } from "../../password_reset/infra"
 
 import { PasswordResetSessionComponent, PasswordResetSessionWorkerComponentHelper } from "../../auth/password_reset_session/action"
 
 export function newPasswordResetSessionComponent(): PasswordResetSessionComponent {
     return initPasswordResetSessionComponent({
-        passwordReset: initPasswordResetAction({
-            timeConfig: newTimeConfig(),
-            passwordResetSessionClient: newPasswordResetSessionClient(),
-            passwordResetClient: newPasswordResetClient(),
-        }),
+        passwordReset: newPasswordResetAction(),
         loginIDField: initLoginIDFieldAction(),
     })
 }
 
 export function newWorkerHelper(): PasswordResetSessionWorkerComponentHelper {
     return initPasswordResetSessionWorkerComponentHelper()
-}
-
-function newPasswordResetSessionClient(): PasswordResetSessionClient {
-    //return initFetchPasswordResetSessionClient(initAuthClient(env.authServerURL))
-    return initSimulatePasswordResetSessionClient({ loginID: "loginID" })
-}
-
-function newPasswordResetClient(): PasswordResetClient {
-    //return initFetchPasswordResetClient(initAuthClient(env.authServerURL))
-    return initSimulatePasswordResetClient(
-        { loginID: "loginID" },
-        {
-            ticketNonce: { ticketNonce: "nonce" },
-            apiCredential: {
-                apiRoles: { apiRoles: ["admin", "dev"] },
-            },
-        },
-    )
 }
