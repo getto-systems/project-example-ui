9f84f402224dd9bbd0d2f6d9e2d2a98c2d34edd6
shun <shun.fix9@gmail.com>
2020-09-21 02:46:18 +0000

    refactor: script

--- public/lib/script/data.ts
+++ public/lib/script/data.ts
@@ -1,5 +1,22 @@
-export type PagePathname = Readonly<{ pagePathname: Readonly<string> }>
-export type ScriptPath = Readonly<{ scriptPath: Readonly<string> }>
+export type PagePathname = Readonly<_PagePathname>
+
+export function initPagePathname(url: Readonly<URL>): PagePathname {
+    return url.pathname as _PagePathname
+}
+
+type _PagePathname = string & { PagePathname: never }
+
+export type ScriptPath = Readonly<_ScriptPath>
+
+export function initScriptPath(scriptPath: string): ScriptPath {
+    return scriptPath as _ScriptPath
+}
+
+export function scriptPathToString(scriptPath: ScriptPath): Readonly<string> {
+    return scriptPath as unknown as string
+}
+
+type _ScriptPath = string & { ScriptPath: never }
 
 export type CheckError =
     Readonly<{ type: "not-found" }> |
