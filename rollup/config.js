202a4d9a14815334d982cdf048413591ca309b50
shun <shun.fix9@gmail.com>
2020-09-30 14:17:39 +0000

    fix: build config

--- rollup/config.js
+++ rollup/config.js
@@ -3,17 +3,35 @@ import { options } from "./options"
 
 const path = require("path");
 
+const root = path.join(__dirname, "../public/lib")
+
 const entrypoint = [
-  { "public/update": path.join(__dirname, "../public/lib/x_update/update.ts") },
-
-  { "public/auth": path.join(__dirname, "../public/lib/x_preact/auth.ts") },
-  { "public/auth/password_login": path.join(__dirname, "../public/lib/x_worker/auth/password_login.ts") },
-  { "public/auth/password_reset_session": path.join(__dirname, "../public/lib/x_worker/auth/password_reset_session.ts") },
-  { "public/auth/password_reset": path.join(__dirname, "../public/lib/x_worker/auth/password_reset.ts") },
-
-  { "secure/home": path.join(__dirname, "../secure/lib/x_preact/home.ts") },
+  {
+    root: "public",
+    entries: [
+      { type: "x_update", names: [ "update" ] },
+      { type: "x_preact", names: [ "auth" ] },
+      { type: "x_worker", names: [
+        "auth/password_login",
+        "auth/password_reset_session",
+        "auth/password_reset",
+      ] },
+    ],
+  },
+  {
+    root: "secure",
+    entries: [
+      { type: "x_preact", names: [ "home" ] },
+    ],
+  },
 ]
 
-export default entrypoint.map((input) => {
-  return { input, ...options };
+export default entrypoint.flatMap((info) => {
+  return info.entries.flatMap((entry) => {
+    return entry.names.map((name) => {
+      const input = {};
+      input[`${info.root}/${name}`] = path.join(__dirname, `../${info.root}/lib/${entry.type}/${name}.ts`);
+      return { input, ...options };
+    })
+  });
 })
