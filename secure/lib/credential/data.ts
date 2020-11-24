86c763df94196f481b2f5c263cc5955d4edac89b
shun <shun.fix9@gmail.com>
2020-11-24 22:05:55 +0000

    refactor: mark

--- secure/lib/credential/data.ts
+++ secure/lib/credential/data.ts
@@ -1,2 +1,9 @@
-export type ApiNonce = { ApiNonce: never }
-export type ApiRoles = { ApiRoles: never }
+export type ApiNonce = string & { ApiNonce: never }
+export function markApiNonce(nonce: string): ApiNonce {
+    return nonce as ApiNonce
+}
+
+export type ApiRoles = string[] & { ApiRoles: never }
+export function markApiRoles(roles: string[]): ApiRoles {
+    return roles as ApiRoles
+}
