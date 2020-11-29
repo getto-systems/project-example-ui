0ac704623d6c3dbc0791b64137a79d6a2e735a2b
shun <shun.fix9@gmail.com>
2020-11-29 21:38:23 +0000

    refactor: main

--- secure/lib/z_main/index/main.ts
+++ secure/lib/z_main/index/main.ts
@@ -2,7 +2,7 @@ import { render, h } from "preact"
 
 import { newDashboardAsSingle } from "../../common/Home/Dashboard/main"
 
-import { Dashboard } from "../common/Home/Dashboard"
+import { Dashboard } from "../../x_preact/common/Home/Dashboard"
 
 render(
     h(Dashboard, { factory: newDashboardAsSingle(localStorage, location) }),
