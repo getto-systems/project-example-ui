d1948067bfae6a9e4766d0db4b1ac67afcc96ca8
shun <shun.fix9@gmail.com>
2020-12-04 00:17:27 +0000

    refactor: usecase

--- public/lib/z_main/login/main.ts
+++ public/lib/z_main/login/main.ts
@@ -2,6 +2,6 @@ import { render, h } from "preact"
 
 import { newLoginAsWorkerForeground } from "../../auth/Auth/Login/main"
 
-import { Login } from "../../x_preact/auth/login/Login"
+import { Login } from "../../x_preact/auth/Auth/Login"
 
 render(h(Login, { login: newLoginAsWorkerForeground() }), document.body)
--- public/lib/z_main/login/mock.ts
+++ public/lib/z_main/login/mock.ts
@@ -2,6 +2,6 @@ import { render, h } from "preact"
 
 import { newLoginAsMock } from "../../auth/Auth/Login/mock"
 
-import { Login } from "../../x_preact/auth/login/Login"
+import { Login } from "../../x_preact/auth/Auth/Login"
 
 render(h(Login, { login: newLoginAsMock() }), document.body)
