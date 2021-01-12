3b558e375fe36965bc65aad84c4986d135631476
shun <shun.fix9@gmail.com>
2021-01-12 04:42:15 +0000

    update: storybook

--- .docker-wrapper.sh
+++ .docker-wrapper.sh
@@ -4,11 +4,7 @@ DOCKER_WRAPPER_IMAGE_debian(){ echo "buster"; }
 DOCKER_WRAPPER_APP(){
   npm-server http $1 npm start
 }
-DOCKER_WRAPPER_STORYBOOK(){
-  npm-server storybook $1 npm run storybook
-}
 DOCKER_WRAPPER_LOGS(){
   npm-server http logs
 }
 DOCKER_WRAPPER_SERVER_OPTS_http(){ echo "-p ${LABO_PORT_PREFIX}${PUBLIC_PORT}:${PUBLIC_APP_PORT} -p ${LABO_PORT_PREFIX}${SECURE_PORT}:${SECURE_APP_PORT}"; }
-DOCKER_WRAPPER_SERVER_OPTS_storybook(){ echo "-p ${LABO_PORT_PREFIX}${STORYBOOK_PORT}:6006"; }
