95c18cd6c0b8a27790cd1ae83bcdef009620fcb5
shun <shun.fix9@gmail.com>
2021-02-10 23:07:15 +0000

    fix: mock

--- example/lib/update/Update/MoveToNextVersion/mock.ts
+++ example/lib/update/Update/MoveToNextVersion/mock.ts
@@ -1,38 +1,17 @@
-import { MockComponent_legacy } from "../../../sub/getto-example/application/mock"
-
-import { initMockNextVersionComponent } from "../nextVersion/mock"
+import { initMockNextVersionComponent, NextVersionMockPropsPasser } from "../nextVersion/mock"
 
 import { MoveToNextVersionEntryPoint } from "./entryPoint"
-import { initialNextVersionComponentState, NextVersionComponentState } from "../nextVersion/component"
 
-export function newMockMoveToNextVersion(): MoveToNextVersionMockEntryPoint {
+export function newMockMoveToNextVersion(
+    passer: NextVersionMockPropsPasser
+): MoveToNextVersionEntryPoint {
     const resource = {
-        nextVersion: initMockNextVersionComponent(initialNextVersionComponentState),
+        nextVersion: initMockNextVersionComponent(passer),
     }
     return {
-        moveToNextVersion: {
-            resource,
-            terminate: () => {
-                // mock では特に何もしない
-            },
-        },
-        update: {
-            nextVersion: update(resource.nextVersion),
+        resource,
+        terminate: () => {
+            // mock では特に何もしない
         },
     }
-}
-
-export type MoveToNextVersionMockEntryPoint = Readonly<{
-    moveToNextVersion: MoveToNextVersionEntryPoint
-    update: Readonly<{
-        nextVersion: Post<NextVersionComponentState>
-    }>
-}>
-
-function update<S, C extends MockComponent_legacy<S>>(component: C): Post<S> {
-    return (state) => component.update(state)
-}
-
-interface Post<T> {
-    (state: T): void
 }
