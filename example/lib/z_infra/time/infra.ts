16e0d2c8af393129df5f96b3adf9397b371b5956
shun <shun.fix9@gmail.com>
2021-02-11 17:47:07 +0000

    fix: infra

--- example/lib/z_infra/time/infra.ts
+++ example/lib/z_infra/time/infra.ts
@@ -1,8 +1,8 @@
 export type DelayTime = Readonly<{ delay_millisecond: number }>
 export type ExpireTime = Readonly<{ expire_millisecond: number }>
 export type IntervalTime = Readonly<{ interval_millisecond: number }>
-export type Limit = Readonly<{ limit: number }>
 export type WaitTime = Readonly<{ wait_millisecond: number }>
+export type Limit = Readonly<{ limit: number }>
 
 export function expireSecond(second: number): ExpireTime {
     return { expire_millisecond: second * 1000 }
