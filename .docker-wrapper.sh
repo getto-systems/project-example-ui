3b627ad994085822b5c0908992c3e12d11403d5c
shun <shun.fix9@gmail.com>
2021-02-20 10:16:24 +0000

    fix: dev-app setting

--- .docker-wrapper.sh
+++ .docker-wrapper.sh
@@ -2,9 +2,12 @@ DOCKER_WRAPPER_IMAGE_node(){ head -1 $APP_ROOT/.gitlab-ci.yml | sed "s/.*node:\(
 DOCKER_WRAPPER_IMAGE_debian(){ echo "buster"; }
 
 DOCKER_WRAPPER_APP(){
-  npm-server http $1 npm start
-}
-DOCKER_WRAPPER_LOGS(){
-  npm-server http logs
+  case $1 in
+    *start)
+      node-run npm run setup
+      ;;
+  esac
+  npm-server http-public $1 npm run start:public
+  npm-server http-secure $1 npm run start:secure
 }
 DOCKER_WRAPPER_SERVER_OPTS_http(){ echo "-p ${LABO_PORT_PREFIX}${PUBLIC_PORT}:${PUBLIC_APP_PORT} -p ${LABO_PORT_PREFIX}${SECURE_PORT}:${SECURE_APP_PORT}"; }
