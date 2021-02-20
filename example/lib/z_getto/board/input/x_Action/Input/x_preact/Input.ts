515f74af38489f986ce0c401f61c0f5557ceb94b
shun <shun.fix9@gmail.com>
2021-02-20 22:41:00 +0000

    refactor: input mock

--- example/lib/z_getto/board/input/x_Action/Input/x_preact/Input.ts
+++ example/lib/z_getto/board/input/x_Action/Input/x_preact/Input.ts
@@ -1,21 +1,26 @@
 import { html } from "htm/preact"
-import { VNode } from "preact"
+import { h, VNode } from "preact"
 
 import { useApplicationAction } from "../../../../../../x_preact/common/hooks"
 
-import { initialInputBoardState, InputBoardValueResource } from "../action"
+import { initialInputBoardState, InputBoardValueResource, InputBoardValueState } from "../action"
 
 import { markBoardValue } from "../../../../kernel/data"
 
-export type InputBoardProps = InputBoardValueResource
-export function InputBoard(resource: InputBoardProps): VNode {
-    const state = useApplicationAction(resource.input, initialInputBoardState)
+export function InputBoard(resource: InputBoardValueResource): VNode {
+    return h(View, {
+        ...resource,
+        state: useApplicationAction(resource.input, initialInputBoardState),
+    })
+}
 
-    return html`<input type=${resource.type} value=${state} onInput=${onInput} />`
+export type InputBoardProps = InputBoardValueResource & Readonly<{ state: InputBoardValueState }>
+export function View(props: InputBoardProps): VNode {
+    return html`<input type=${props.type} value=${props.state} onInput=${onInput} />`
 
     function onInput(event: InputEvent) {
         if (event.target instanceof HTMLInputElement) {
-            resource.input.set(markBoardValue(event.target.value))
+            props.input.set(markBoardValue(event.target.value))
         }
     }
 }
