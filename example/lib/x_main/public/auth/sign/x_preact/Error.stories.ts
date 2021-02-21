28670a3ed6769b9687e9939930a5ad556f08a59e
shun <shun.fix9@gmail.com>
2021-02-21 22:34:57 +0000

    move: EntryPoint

--- example/lib/x_main/public/auth/sign/x_preact/Error.stories.ts
+++ example/lib/x_main/public/auth/sign/x_preact/Error.stories.ts
@@ -1,17 +1,17 @@
 import { h, VNode } from "preact"
 import { useEffect } from "preact/hooks"
 
-import { storyTemplate } from "../../../z_vendor/storybook/preact/story"
-import { noPaddedStory } from "../../../z_vendor/storybook/preact/display"
+import { storyTemplate } from "../../../../../z_vendor/storybook/preact/story"
+import { noPaddedStory } from "../../../../../z_vendor/storybook/preact/display"
 
 import { EntryPoint } from "./EntryPoint"
 
-import { initMockPropsPasser } from "../../../z_getto/application/mock"
+import { initMockPropsPasser } from "../../../../../z_getto/application/mock"
 
 import {
     LoginErrorMockProps,
     initMockLoginEntryPointAsError,
-} from "../../../x_main/public/auth/sign/mock"
+} from "../mock"
 
 export default {
     title: "Auth/Sign/Error",
