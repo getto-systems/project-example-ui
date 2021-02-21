bcc4cef752a723d54d5ee59aea2e7023260c63a3
shun <shun.fix9@gmail.com>
2021-02-21 20:28:28 +0000

    refactor: use board value store hook

--- example/lib/z_getto/board/input/x_Action/Input/x_preact/Input.ts
+++ example/lib/z_getto/board/input/x_Action/Input/x_preact/Input.ts
@@ -2,24 +2,33 @@ import { VNode } from "preact"
 import { useLayoutEffect, useRef } from "preact/hooks"
 import { html } from "htm/preact"
 
-import { InputBoardValueResource } from "../action"
+import { InputBoardValueAction, InputBoardValueResource } from "../action"
 
 import { BoardValue, emptyBoardValue, markBoardValue } from "../../../../kernel/data"
 
 export type InputBoardProps = InputBoardValueResource
 export function InputBoard(props: InputBoardProps): VNode {
-    const REF = useRef<HTMLInputElement>()
-    useLayoutEffect(() => {
-        props.input.linkStore({
-            get,
-            set,
-        })
-    }, [])
-    return html`<input ref=${REF} type=${props.type} onInput=${onInput} />`
+    return html`<input
+        ref=${useBoardValueStore(props.input)}
+        type=${props.type}
+        onInput=${onInput}
+    />`
 
     function onInput() {
         props.input.triggerInputEvent()
     }
+}
+
+function useBoardValueStore(input: InputBoardValueAction) {
+    const REF = useRef<HTMLInputElement>()
+    useLayoutEffect(() => {
+        input.linkStore({
+            get,
+            set,
+        })
+    }, [])
+
+    return REF
 
     function get() {
         if (!REF.current) {
