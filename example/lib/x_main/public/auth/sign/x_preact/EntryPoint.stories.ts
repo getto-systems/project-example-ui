be83623e23dbc69c350b83a69ac77374e7d7d698
shun <shun.fix9@gmail.com>
2021-02-21 22:37:17 +0000

    fix: EntryPoint story

--- example/lib/x_main/public/auth/sign/x_preact/EntryPoint.stories.ts
+++ example/lib/x_main/public/auth/sign/x_preact/EntryPoint.stories.ts
@@ -2,19 +2,18 @@ import { h, VNode } from "preact"
 import { useEffect } from "preact/hooks"
 
 import { storyTemplate } from "../../../../../z_vendor/storybook/preact/story"
-import { noPaddedStory } from "../../../../../z_vendor/storybook/preact/display"
 
 import { EntryPoint } from "./EntryPoint"
 
 import { initMockPropsPasser } from "../../../../../z_getto/application/mock"
 
-import {
-    LoginErrorMockProps,
-    initMockLoginEntryPointAsError,
-} from "../mock"
+import { LoginErrorMockProps, initMockLoginEntryPointAsError } from "../mock"
 
 export default {
-    title: "Auth/Sign/Error",
+    title: "main/Auth/Sign",
+    parameters: {
+        layout: "fullscreen",
+    },
 }
 
 type MockProps = LoginErrorMockProps
@@ -27,7 +26,7 @@ const template = storyTemplate<MockProps>((args) => {
         useEffect(() => {
             passer.update(props.args)
         })
-        return noPaddedStory(h(EntryPoint, entryPoint))
+        return h(EntryPoint, entryPoint)
     }
 })
 
