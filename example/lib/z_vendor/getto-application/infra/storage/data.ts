21beca3340260cca1c9ff14bda8deb7b2dc0e217
shun <shun.fix9@gmail.com>
2021-03-03 18:17:00 +0000

    refactor: storage

--- example/lib/z_vendor/getto-application/infra/storage/data.ts
+++
@@ -1,5 +0,0 @@
-export type StorageError = Readonly<{ type: "infra-error"; err: string }>
-
-export type StoreResult =
-    | Readonly<{ success: true }>
-    | Readonly<{ success: false; err: StorageError }>
