4f487f99f3b6ab260c7f76b7bedeccaa1ea51688
shun <shun.fix9@gmail.com>
2021-03-05 21:55:36 +0000

    refactor: request reset token test

--- example/lib/auth/sign/password/reset/requestToken/Action/test.ts
+++ example/lib/auth/sign/password/reset/requestToken/Action/test.ts
@@ -1,93 +1,98 @@
 import {
-    initAsyncActionTester_legacy,
+    initAsyncActionTestRunner,
     initSyncActionTestRunner,
 } from "../../../../../../z_vendor/getto-application/action/testHelper"
+
 import { standardBoardValueStore } from "../../../../../../z_vendor/getto-application/board/input/Action/testHelper"
 import { markBoardValue } from "../../../../../../z_vendor/getto-application/board/kernel/testHelper"
+import { initRemoteSimulator } from "../../../../../../z_vendor/getto-application/infra/remote/simulate"
 
+import { toRequestResetTokenEntryPoint } from "./impl"
 import { initRequestResetTokenCoreMaterial, initRequestResetTokenCoreAction } from "./Core/impl"
 import { initRequestResetTokenFormAction } from "./Form/impl"
 
 import { requestResetTokenEventHasDone } from "../impl/core"
+import { resetSessionIDRemoteConverter } from "../../kernel/convert"
 
 import { RequestResetTokenRemotePod, RequestResetTokenResult } from "../infra"
 
 import { RequestResetTokenEntryPoint } from "./action"
 import { RequestResetTokenCoreState } from "./Core/action"
 
-import { resetSessionIDRemoteConverter } from "../../kernel/convert"
-
-import { toRequestResetTokenEntryPoint } from "./impl"
-import { initRemoteSimulator } from "../../../../../../z_vendor/getto-application/infra/remote/simulate"
-
 const VALID_LOGIN = { loginID: "login-id" } as const
 
 describe("RequestResetToken", () => {
     test("submit valid login-id", (done) => {
-        const { entryPoint } = standardPasswordResetSessionResource()
+        const { entryPoint } = standard_elements()
         const resource = entryPoint.resource.requestToken
 
-        resource.core.subscriber.subscribe(initTester())
+        const runner = initAsyncActionTestRunner(actionHasDone, [
+            {
+                statement: () => {
+                    resource.form.loginID.board.input.set(markBoardValue(VALID_LOGIN.loginID))
 
-        resource.form.loginID.board.input.set(markBoardValue(VALID_LOGIN.loginID))
+                    resource.core.submit(resource.form.validate.get())
+                },
+                examine: (stack) => {
+                    expect(stack).toEqual([
+                        { type: "try-to-request-token" },
+                        { type: "succeed-to-request-token", sessionID: "session-id" },
+                    ])
+                },
+            },
+        ])
 
-        resource.core.submit(resource.form.validate.get())
-
-        function initTester() {
-            return initAsyncTester()((stack) => {
-                expect(stack).toEqual([
-                    { type: "try-to-request-token" },
-                    { type: "succeed-to-request-token", sessionID: "session-id" },
-                ])
-                done()
-            })
-        }
+        resource.core.subscriber.subscribe(runner(done))
     })
 
     test("submit valid login-id; with delayed", (done) => {
         // wait for delayed timeout
-        const { entryPoint } = waitPasswordResetSessionResource()
+        const { entryPoint } = takeLongTime_elements()
         const resource = entryPoint.resource.requestToken
 
-        resource.core.subscriber.subscribe(initTester())
+        const runner = initAsyncActionTestRunner(actionHasDone, [
+            {
+                statement: () => {
+                    resource.form.loginID.board.input.set(markBoardValue(VALID_LOGIN.loginID))
 
-        resource.form.loginID.board.input.set(markBoardValue(VALID_LOGIN.loginID))
+                    resource.core.submit(resource.form.validate.get())
+                },
+                examine: (stack) => {
+                    expect(stack).toEqual([
+                        { type: "try-to-request-token" },
+                        { type: "delayed-to-request-token" }, // delayed event
+                        { type: "succeed-to-request-token", sessionID: "session-id" },
+                    ])
+                },
+            },
+        ])
 
-        resource.core.submit(resource.form.validate.get())
-
-        function initTester() {
-            return initAsyncTester()((stack) => {
-                expect(stack).toEqual([
-                    { type: "try-to-request-token" },
-                    { type: "delayed-to-request-token" }, // delayed event
-                    { type: "succeed-to-request-token", sessionID: "session-id" },
-                ])
-                done()
-            })
-        }
+        resource.core.subscriber.subscribe(runner(done))
     })
 
     test("submit without fields", (done) => {
-        const { entryPoint } = standardPasswordResetSessionResource()
+        const { entryPoint } = standard_elements()
         const resource = entryPoint.resource.requestToken
 
-        resource.core.subscriber.subscribe(initTester())
+        const runner = initAsyncActionTestRunner(actionHasDone, [
+            {
+                statement: () => {
+                    // try to request token without fields
+                    resource.core.submit(resource.form.validate.get())
+                },
+                examine: (stack) => {
+                    expect(stack).toEqual([
+                        { type: "failed-to-request-token", err: { type: "validation-error" } },
+                    ])
+                },
+            },
+        ])
 
-        // try to request token without fields
-        resource.core.submit(resource.form.validate.get())
-
-        function initTester() {
-            return initAsyncTester()((stack) => {
-                expect(stack).toEqual([
-                    { type: "failed-to-request-token", err: { type: "validation-error" } },
-                ])
-                done()
-            })
-        }
+        resource.core.subscriber.subscribe(runner(done))
     })
 
     test("clear", () => {
-        const { entryPoint } = standardPasswordResetSessionResource()
+        const { entryPoint } = standard_elements()
         const resource = entryPoint.resource.requestToken
 
         resource.form.loginID.board.input.set(markBoardValue(VALID_LOGIN.loginID))
@@ -97,7 +102,7 @@ describe("RequestResetToken", () => {
     })
 
     test("terminate", (done) => {
-        const { entryPoint } = standardPasswordResetSessionResource()
+        const { entryPoint } = standard_elements()
         const resource = entryPoint.resource.requestToken
 
         const runner = initSyncActionTestRunner([
@@ -121,20 +126,18 @@ describe("RequestResetToken", () => {
     })
 })
 
-function standardPasswordResetSessionResource() {
-    const entryPoint = newTestPasswordResetSessionResource(standard_requestToken())
+function standard_elements() {
+    const entryPoint = newEntryPoint(standard_requestToken())
 
     return { entryPoint }
 }
-function waitPasswordResetSessionResource() {
-    const entryPoint = newTestPasswordResetSessionResource(takeLongTime_requestToken())
+function takeLongTime_elements() {
+    const entryPoint = newEntryPoint(takeLongTime_requestToken())
 
     return { entryPoint }
 }
 
-function newTestPasswordResetSessionResource(
-    requestToken: RequestResetTokenRemotePod,
-): RequestResetTokenEntryPoint {
+function newEntryPoint(requestToken: RequestResetTokenRemotePod): RequestResetTokenEntryPoint {
     const entryPoint = toRequestResetTokenEntryPoint({
         core: initRequestResetTokenCoreAction(
             initRequestResetTokenCoreMaterial({
@@ -165,17 +168,12 @@ function simulateRequestToken(): RequestResetTokenResult {
     return { success: true, value: resetSessionIDRemoteConverter("session-id") }
 }
 
-function initAsyncTester() {
-    return initAsyncActionTester_legacy((state: RequestResetTokenCoreState) => {
-        switch (state.type) {
-            case "initial-request-token":
-                return false
+function actionHasDone(state: RequestResetTokenCoreState): boolean {
+    switch (state.type) {
+        case "initial-request-token":
+            return false
 
-            case "try-to-request-token":
-            case "delayed-to-request-token":
-            case "failed-to-request-token":
-            case "succeed-to-request-token":
-                return requestResetTokenEventHasDone(state)
-        }
-    })
+        default:
+            return requestResetTokenEventHasDone(state)
+    }
 }
