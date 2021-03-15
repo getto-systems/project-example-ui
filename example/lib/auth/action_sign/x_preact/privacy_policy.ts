f40d7916bae0f5b430b7692b6855358c108fbc6f
shun <shun.fix9@gmail.com>
2021-03-15 21:55:00 +0000

    add: privacy policy

--- example/lib/auth/action_sign/x_preact/privacy_policy.ts
+++ example/lib/auth/action_sign/x_preact/privacy_policy.ts
@@ -2,40 +2,20 @@ import { VNode } from "preact"
 import { html } from "htm/preact"
 
 import { loginBox } from "../../../z_vendor/getto-css/preact/layout/login"
-import { buttons, field } from "../../../z_vendor/getto-css/preact/design/form"
+import { buttons } from "../../../z_vendor/getto-css/preact/design/form"
 
 import { siteInfo } from "../../../x_preact/common/site"
 import { signNav } from "../../sign/common/nav/x_preact/nav"
+import { docsSectionBody } from "../../../docs/action_docs/x_preact/content"
+
+import { docs_privacyPolicy } from "../../../docs/docs"
 
 import { SignLinkResource } from "../../sign/common/nav/action_nav/resource"
 
 export function PrivacyPolicyComponent(props: SignLinkResource): VNode {
     return loginBox(siteInfo(), {
         title: "プライバシーポリシー",
-        body: [
-            field({
-                title: "ログインID / パスワード",
-                body: html`
-                    システムを使用するための認証に使用します<br />
-                    それ以外の用途で使用することはありません<br />
-                    パスワードは暗号化して送信、保存されます
-                `,
-            }),
-            field({
-                title: "メールアドレス",
-                body: html`
-                    パスワードリセットのために使用します<br />
-                    それ以外の用途で使用することはありません
-                `,
-            }),
-            field({
-                title: "業務データ",
-                body: html`
-                    システムで扱うすべてのデータは、業務を行うために使用します<br />
-                    業務に関係ない対象に情報が開示されることはありません
-                `,
-            }),
-        ],
+        body: docs_privacyPolicy.flatMap((section) => html`${section.body.map(docsSectionBody)}`),
         footer: buttons({ left: loginLink(), right: resetLink() }),
     })
 
---
+++ example/lib/x_main/secure/docs/privacy_policy/foreground.ts
@@ -0,0 +1,22 @@
+import { render, h } from "preact"
+
+import { newDocsView } from "../../../../docs/action_docs/init"
+
+import { docs_privacyPolicy } from "../../../../docs/docs"
+
+import { DocsEntry } from "../../../../docs/action_docs/x_preact/docs"
+
+render(
+    h(
+        DocsEntry,
+        newDocsView({
+            webStorage: localStorage,
+            currentLocation: location,
+            docs: {
+                title: "プライバシーポリシー",
+                contents: [[docs_privacyPolicy]],
+            },
+        }),
+    ),
+    document.body,
+)
