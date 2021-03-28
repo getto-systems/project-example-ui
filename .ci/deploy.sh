93fa96d7bdd916a0e9db2812fb01314592459848
shun <shun.fix9@gmail.com>
2021-03-28 05:04:23 +0000

    fix: deploy

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -9,8 +9,7 @@ deploy_main(){
   deploy_cp_public
   deploy_cp_secure
 
-  deploy_cp_public_index "0.8.0"
-  deploy_cp_public_index "0.8.1"
+  #deploy_cp_public_index "0.8.0"
 }
 deploy_build_ui(){
   npm run build
