8a0f324204a6db046b8e233e05ce3c61be4895a9
shun <shun.fix9@gmail.com>
2021-03-29 22:24:30 +0000

    refactor: unused code

--- lib/avail/action_notify_unexpected_error/init.ts
+++ lib/avail/action_notify_unexpected_error/init.ts
@@ -1,23 +1,18 @@
-import { newAuthzRepository } from "../../auth/auth_ticket/kernel/infra/repository/authz"
-import { newNotifyUnexpectedErrorRemote } from "../notify_unexpected_error/infra/remote/notify"
+import { newNotifyUnexpectedErrorInfra } from "../notify_unexpected_error/impl/init"
 
+import { initNotifyUnexpectedErrorResource } from "./impl"
 import { initNotifyUnexpectedErrorCoreAction } from "./core/impl"
 
 import { NotifyUnexpectedErrorResource } from "./resource"
-import { initNotifyUnexpectedErrorResource } from "./impl"
 
 type OutsideFeature = Readonly<{
-    webStorage: Storage
     webCrypto: Crypto
 }>
 export function newNotifyUnexpectedErrorResource(
     feature: OutsideFeature,
 ): NotifyUnexpectedErrorResource {
-    const { webStorage, webCrypto } = feature
+    const { webCrypto } = feature
     return initNotifyUnexpectedErrorResource(
-        initNotifyUnexpectedErrorCoreAction({
-            authz: newAuthzRepository(webStorage),
-            notify: newNotifyUnexpectedErrorRemote(webCrypto),
-        }),
+        initNotifyUnexpectedErrorCoreAction(newNotifyUnexpectedErrorInfra(webCrypto)),
     )
 }
