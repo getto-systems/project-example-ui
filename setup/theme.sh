8606aa196f35b0c11a4c46a6a36a86672ad6772e
shun <shun.fix9@gmail.com>
2021-04-01 07:12:16 +0000

    update: theme

--- setup/theme.sh
+++ setup/theme.sh
@@ -4,6 +4,6 @@ if [ -z "$APP_ROOT" ]; then
     APP_ROOT=.
 fi
 
-version=2.28.2
+version=2.29.0
 find $APP_ROOT/public/ -name '*.html' | xargs sed -i "s|[^/]*/getto.css|${version}/getto.css|"
 find $APP_ROOT/storybook/ -name '*.html' | xargs sed -i "s|[^/]*/getto.css|${version}/getto.css|"
