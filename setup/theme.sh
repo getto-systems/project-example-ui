56eb83164539e8473599c84a7275559c2457e1f8
shun <shun.fix9@gmail.com>
2021-04-30 09:03:55 +0900

    fix: setup script

--- setup/theme.sh
+++ setup/theme.sh
@@ -4,9 +4,13 @@ if [ -z "$APP_ROOT" ]; then
     APP_ROOT=.
 fi
 
+ext='.theme-backup'
+
 version=2.34.0
-find $APP_ROOT/public -name '*.html' | xargs sed -i '' "s|[^/]*/getto.css|${version}/getto.css|"
-find $APP_ROOT/storybook -name '*.html' | xargs sed -i '' "s|[^/]*/getto.css|${version}/getto.css|"
+find $APP_ROOT/public -name '*.html' | xargs sed -i $ext "s|[^/]*/getto.css|${version}/getto.css|"
+find $APP_ROOT/storybook -name '*.html' | xargs sed -i $ext "s|[^/]*/getto.css|${version}/getto.css|"
 
 title="GETTO Example"
-find $APP_ROOT/public -name '*.html' | xargs sed -i '' "s|<title>.*</title>|<title>${title}</title>|"
+find $APP_ROOT/public -name '*.html' | xargs sed -i $ext "s|<title>.*</title>|<title>${title}</title>|"
+
+find $APP_ROOT -name '*.html'$ext | xargs rm
